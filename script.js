cf5c506bebe7bad5405883f82c77da38d9d2d967
alena-resh <reshetnikovaalenna@gmail.com>
2024-08-17 16:24:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//прототипом может быть только объект
+
+let a = { 
+    "text":'Hello hello',
+    "color": 'red',
+    "bold": true,
+    "show": function (){
+        console.log(this.color);
+    }
+};
+
+let b = {
+    "fontSize": '24px', 
+    __proto__:  a //наследник а
+}
+
+let c = {
+    "fontFamily": 'Verdana', 
+    __proto__ : b //наследник b
+}
+
+console.log(c.bold);
